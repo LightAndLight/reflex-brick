521b8710fd63a47d2d6896cf5495d3cf45b8c34d
Isaac Elliott <isaace71295@gmail.com>
2019-03-21 11:39:56 +1000

    Added checkboxes and radios

--- examples/forms/Main.hs
+++ examples/forms/Main.hs
@@ -1,6 +1,7 @@
 {-# language DeriveFunctor, StandaloneDeriving #-}
 {-# language DerivingVia #-}
 {-# language GADTs #-}
+{-# language OverloadedLists #-}
 {-# language OverloadedStrings #-}
 {-# language RecursiveDo #-}
 {-# language ScopedTypeVariables #-}
@@ -17,26 +18,32 @@ This will likely be factored out into a library, but I'm leaving it here for now
 import Reflex
 import Reflex.Brick
 
-import Brick.AttrMap (attrMap)
+import Brick.AttrMap (AttrMap, attrMap)
 import Brick.Focus
   (FocusRing, focusRing, focusRingCursor, focusNext, focusPrev, focusGetCurrent)
+import Brick.Forms (focusedFormInputAttr)
 import Brick.Widgets.Border (border)
-import Brick.Widgets.Core ((<=>), (<+>), emptyWidget, txt, vLimit)
+import Brick.Widgets.Core
+  ((<=>), (<+>), emptyWidget, txt, vLimit, vBox, withDefAttr, showCursor, padBottom)
 import Brick.Widgets.Edit
   (Editor, editorText, editContentsL, renderEditor, getEditContents)
-import Brick.Types (Widget)
+import Brick.Types (Widget, Location(..), Padding(..))
+import Brick.Util (on)
 import Control.Monad (guard)
 import Control.Monad.Fix (MonadFix)
 import Control.Monad.Trans.Class (lift)
 import Control.Monad.Reader (ReaderT, runReaderT, ask)
 import Data.Function ((&))
+import Data.List.NonEmpty (NonEmpty(..))
 import Data.Maybe (fromMaybe)
 import Data.Text (Text)
-import Lens.Micro ((%~), mapped)
+import Lens.Micro ((%~), (^.), _1, _2, mapped, _last)
 import Lens.Micro.TH (makeLenses)
 import qualified Data.Text.Zipper as Zipper
 
+import qualified Data.List.NonEmpty as NonEmpty
 import qualified Data.Text as Text
+import qualified Graphics.Vty.Attributes as Vty
 import qualified Graphics.Vty.Input.Events as Vty
 
 -- | From 'Brick.Widgets.Edit.handleEditorEvent'
@@ -161,11 +168,12 @@ makeForm ::
   ( Reflex t, MonadHold t m, MonadFix m
   , Eq n
   ) =>
+  AttrMap ->
   Event t () ->
   Event t () ->
   Form t n m a ->
   m (Dynamic t a, Dynamic t (Maybe n), Dynamic t (ReflexBrickAppState n))
-makeForm ePrev eNext form = do
+makeForm style ePrev eNext form = do
   rec
     let dFocus = focusGetCurrent <$> dFocusRing
     ff <- runReaderT (go form) dFocus
@@ -177,7 +185,7 @@ makeForm ePrev eNext form = do
        ReflexBrickAppState
        { _rbWidgets = [w]
        , _rbCursorFn = focusRingCursor id fr
-       , _rbAttrMap = attrMap mempty []
+       , _rbAttrMap = style
        }) <$>
       _fieldWidget ff <*>
       dFocusRing
@@ -218,9 +226,14 @@ textFieldBase eVtyEvent renderLines name def limit dFocus = do
   pure $
     FormField
     { _fieldData = getEditContents <$> dEditor
-    , _fieldWidget = renderEditor renderLines <$> dInFocus <*> dEditor
+    , _fieldWidget =
+      (\focus -> addStyle focus . renderEditor renderLines focus) <$>
+      dInFocus <*>
+      dEditor
     , _fieldNames = [name]
     }
+  where
+    addStyle focus = if focus then withDefAttr focusedFormInputAttr else id
 
 textField ::
   forall t m n.
@@ -262,8 +275,107 @@ passwordField eVtyEvent name def =
   where
     stars s = Text.replicate (Text.length $ Text.concat s) (Text.singleton '*')
 
-data FormId = FormName | FormPassword
+radioField ::
+  ( Reflex t, MonadHold t m
+  , Eq n
+  ) =>
+  EventSelector t RBVtyEvent -> -- ^ terminal events
+  NonEmpty (n, a, Text) -> -- ^ selections
+  Dynamic t (Maybe n) -> -- ^ current focus
+  m (FormField t n a)
+radioField eVtyEvent choices dFocus = do
+  let
+    names = foldr (\a b -> (^.) a _1 : b) [] choices
+    initial = let x = NonEmpty.head choices in (x ^. _1, x ^. _2)
+    eSpace = select eVtyEvent (RBKey $ Vty.KChar ' ')
+
+  let dHighlighted = (>>= \n -> (,) n <$> lookupValue n choices) <$> dFocus
+  dSelected <- holdDyn initial $ attachWithMaybe const (current dHighlighted) eSpace
+
+  pure $
+    FormField
+    { _fieldData = (^. _2) <$> dSelected
+    , _fieldWidget =
+        (\mh (ns, _) ->
+           vBox $
+           foldr
+             (\(n', _, a) -> (:) (addStyle mh n' . txt $ radio ns n' <> a))
+             []
+             choices) <$>
+        dHighlighted <*>
+        dSelected
+    , _fieldNames = names
+    }
+  where
+    lookupValue n ((n', a, _) :| []) =
+      if n == n' then Just a else Nothing
+    lookupValue n ((n', a, _) :| (x : xs)) =
+      if n == n' then Just a else lookupValue n (x :| xs)
+
+    addStyle mh n' =
+      if maybe False (\(nh, _) -> nh == n') mh
+      then showCursor n' (Location (1, 0)) . withDefAttr focusedFormInputAttr
+      else id
+
+    radio ns n' = if ns == n' then "[*] " else "[ ] "
+
+checkboxField ::
+  ( Reflex t, MonadHold t m, MonadFix m
+  , Eq n
+  ) =>
+  EventSelector t RBVtyEvent -> -- ^ terminal events
+  n -> -- ^ name
+  Text -> -- ^ label
+  Dynamic t (Maybe n) -> -- ^ current focus
+  m (FormField t n Bool)
+checkboxField eVtyEvent name label dFocus = do
+  let eSpace = select eVtyEvent (RBKey $ Vty.KChar ' ')
+
+  let dHighlighted = (==Just name) <$> dFocus
+  rec
+    dChecked <-
+      holdDyn False $
+      (not <$>
+      current dChecked <@
+      attachWithMaybe
+        (\h _ -> guard h)
+        (current dHighlighted)
+        eSpace)
+
+  pure $
+    FormField
+    { _fieldData = dChecked
+    , _fieldWidget =
+        (\h c -> addStyle h $ txt $ check c <> label) <$>
+        dHighlighted <*>
+        dChecked
+    , _fieldNames = [name]
+    }
+  where
+    addStyle h =
+      if h
+      then showCursor name (Location (1, 0)) . withDefAttr focusedFormInputAttr
+      else id
+
+    check c = if c then "[x] " else "[ ] "
+
+data FormId
+  = FormName
+  | FormPassword
+  | FormX
+  | FormY
+  | FormZ
+  | FormQuestion
   deriving (Eq, Show, Ord)
+
+data C = X | Y | Z
+  deriving (Eq, Show, Ord)
+
+styling :: AttrMap
+styling =
+  attrMap Vty.defAttr
+  [ (focusedFormInputAttr, Vty.black `on` Vty.white)
+  ]
 
 network ::
   (Reflex t, MonadHold t m, MonadFix m) =>
@@ -272,21 +384,37 @@ network ::
 network eEvent = do
   let
     eVtyEvent = fan $ select eEvent RBAnyVtyEvent
-    eTab = select eEvent (RBVtyEvent . RBKey $ Vty.KChar '\t')
-    ePrev = fmapMaybe (\ms -> guard (ms == [Vty.MShift])) eTab
-    eNext = fmapMaybe (\ms -> guard (ms == [])) eTab
+    ePrev =
+      fmapMaybe
+        (guard . null)
+        (select eEvent (RBVtyEvent . RBKey $ Vty.KBackTab))
+    eNext =
+      fmapMaybe
+        (guard . null)
+        (select eEvent (RBVtyEvent . RBKey $ Vty.KChar '\t'))
     eQuit = () <$ select eEvent (RBVtyEvent $ RBKey Vty.KEnter)
 
   rec
-    (_, dFocus, dAppState) <-
-      makeForm ePrev eNext $
-      (,) <$>
+    (dData, dFocus, dAppState) <-
+      makeForm styling ePrev eNext $
+      (,,,) <$>
       styled border (field $ textField eVtyEvent FormName Nothing (Just 1)) <*>=
-      styled border (field $ passwordField eVtyEvent FormPassword Nothing)
+      styled border (field $ passwordField eVtyEvent FormPassword Nothing) <*>=
+      styled
+        (padBottom $ Pad 1)
+        (field (radioField eVtyEvent [(FormX, X, "X"), (FormY, Y, "Y"), (FormZ, Z, "Z")])) <*>=
+      field (checkboxField eVtyEvent FormQuestion "???")
 
   pure $
     ReflexBrickApp
-    { rbaAppState = dAppState
+    { rbaAppState =
+      (\as output ->
+        as
+          { _rbWidgets =
+            _rbWidgets as & _last %~ (<=> txt ("output: " <> Text.pack (show output)))
+          }) <$>
+      dAppState <*>
+      dData
     , rbaSuspendAndResume = never
     , rbaHalt = eQuit
     }
